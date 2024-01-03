c4385d851bd6d316ea77eeac42f18e0129847b89
Walttinho <walter.netto@live.com>
2024-01-03 01:05:07 -0300

    Desafio Resolvido

---
+++ Desafio-02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    string titulo, descricao, dataVencimento;
+
+    titulo = Console.ReadLine();
+    descricao = Console.ReadLine();
+
+    if (descricao.Length > 50)
+    {
+      Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+    }
+    else
+    {
+      dataVencimento = Console.ReadLine();
+
+      Console.WriteLine($"{descricao}" + " ate " + $"{dataVencimento}");
+
+    }
+  }
+}
