89ad5fcb3912425128b773421a7834020db47b37
Walttinho <walter.netto@live.com>
2024-07-13 20:48:36 -0300

    solved

---
+++ Explorando POO com Python/Adicionando Funcionalidades ao Plano/desafio.py
@@ -0,0 +1,42 @@
+# Classe PlanoTelefone
+class PlanoTelefone:
+    def __init__(self, nome, saldo):
+        self.__nome = nome
+        self.__saldo = saldo
+
+    def verificar_saldo(self):
+        return self.__saldo
+
+    def mensagem_personalizada(self):
+        if self.__saldo < 10:
+            return "Seu saldo está baixo. Recarregue e use os serviços do seu plano."
+        elif self.__saldo >= 50:
+            return "Parabéns! Continue aproveitando seu plano sem preocupações."
+        else:
+            return "Seu saldo está razoável. Aproveite o uso moderado do seu plano."
+
+
+# Classe UsuarioTelefone
+class UsuarioTelefone:
+    def __init__(self, nome, plano):
+        self.nome = nome
+        self.plano = plano
+
+    def verificar_saldo(self):
+        saldo_usuario = self.plano.verificar_saldo()
+        mensagem_usuario = self.plano.mensagem_personalizada()
+        return saldo_usuario, mensagem_usuario
+
+
+# Recebendo as entradas do usuário (nome, plano, saldo):
+nome_usuario = input()
+nome_plano = input()
+saldo_inicial = float(input())
+
+# Criação de objetos do plano de telefone e usuário de telefone com dados fornecidos:
+plano_usuario = PlanoTelefone(nome_plano, saldo_inicial) 
+usuario = UsuarioTelefone(nome_usuario, plano_usuario)  
+
+# Chamada do método para verificar_saldo sem acessar diretamente os atributos do plano:
+saldo_usuario, mensagem_usuario = usuario.verificar_saldo()  
+print(mensagem_usuario)
